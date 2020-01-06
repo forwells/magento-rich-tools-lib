0a20f19561c081fcc82d965026d593a1036335f7
tommy <forwelll@163.com>
2020-01-06 16:58:54 +0800

    update-view-and-share-functions

--- src/Tools/View/Block.php
+++ src/Tools/View/Block.php
@@ -4,7 +4,7 @@ namespace Tools\View;
 
 class Block extends \Tools\Base
 {
-	public function insertBlock($template = "", $class = "Magento\Framework\View\Element\Template")
+	public function insertBlock($template, $class = "Magento\Framework\View\Element\Template")
 	{
 		if($template){
 			return $this->getLayout()->createBlock($class)->setTemplate($template)->toHtml();	
@@ -12,5 +12,13 @@ class Block extends \Tools\Base
 			throw new Exception("Please Specified The Template You Want To Use!", 1);
 		}
 		
+	}
+
+	public function cmsBlock($blockName)
+	{
+		$layout = $this->getLayout();
+		$cmsBlock = $layout->createBlock("Magento\Cms\Block\Block")->setBlockId($blockName)->toHtml();
+
+		return $cmsBlock;
 	}
 }
--- src/shared/global.php
+++ src/shared/global.php
@@ -12,7 +12,17 @@ if(!function_exists('d')){
 		echo "</pre>";
 	}
 }
-
+/**
+ * js debug info : 控制台错误信息
+ */
+if(!function_exists('console_log')){
+	function console_log($params = ""){
+		if(!$params){
+			return "<script>console.log('debug params is null or disabled!')</script>";
+		}
+		return "<script>console.log(".$params.")</script>";
+	}
+}
 /**
  * store: 快捷获取Store实例
  */
@@ -155,60 +165,13 @@ if(!function_exists('set_element_order')){
 	}
 }
 
-
-if(!function_exists('store')){
-	function store()
-	{
-		return new \Tools\Store\Store;
-	}
-}
-
-if(!function_exists('account')){
-	function account()
-	{
-		return new \Tools\Account\Account;
-	}
-}
-
-if(!function_exists('view_call')){
-	function view_call()
-	{
-		return new \Tools\View\Call;
-	}
-}
-
-if(!function_exists('route')){
-	function route()
-	{
-		return new \Tools\Router\Router;
-	}
-}
-
-if(!function_exists('current_product')){
-	function current_product()
-	{
-		$product = new \Tools\Product\Product;
-		return $product->getCurrentProduct();
-	}
-}
 /**
- * Block : 快捷在phtml使用Block
+ * cms_block 后台Cms block
  */
-if(!function_exists('block')){
-	function block($templateName = "", $class = "Magento\Framework\View\Element\Template")
+if(!function_exists('cms_block')){
+	function cms_block($blockName = "")
 	{
-		$block = new \Tools\View\Block;
-		return $block->insertBlock($templateName, $class);
-	}
-}
-
-/**
- * Data: 当前产品的评论评分平均分
- */
-if(!function_exists('average_review_rating')){
-	function average_review_rating()
-	{
-		$rating = new \Tools\Review\Summary;
-		return $rating->getAverageReviewVote();
+		$view = new \Tools\View\Block;
+		return $view->cmsBlock($blockName);
 	}
 }
